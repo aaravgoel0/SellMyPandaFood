d0a9f87b98d2bafca298011f833b9c98fbee1e10
Tanush P <148394954+michealjordann@users.noreply.github.com>
2026-02-11 14:57:01 -0800

    Update TargetedAd.java

--- TargetedAd.java
+++ TargetedAd.java
@@ -15,16 +15,10 @@ public class TargetedAd {
      * (2) Create a new DataCollector object and set the data to "socialMediaPostsSmall.txt" and "targetWords.txt"
      *     Important: Use the socialMedialPostsSmall to create your algorithm. Using a small file will help you 
      *     generate your solution quicker and give you the ability to double check your work.
-     * (3) Create a String variable to hold the names of all the user. (The first word of every post is 
-     *     a person's username)
      * (4) Compare each user's post to each target word. If a user mentions a target word, add their username to 
      *     the String of users. Separate usernames with a space. 
      *         Hint: You can use loops to look through each word. 
      *         Hint2: You can use indexOf to check if a word is in a user post. 
-     * (5) Once you have all the users, use your DataCollector's prepareAdvertisement method to prepare a file 
-     *     with all users and the advertisement you will send them.
-     *         Additional Info: The prepareAdvertisement creates a new file on your computer. Check the posts of
-     *         some of the usernames to make sure your algorithm worked.
      * 
      * THE FINAL SOLUTION
      * (6) Your solution should work with the socialMedialPostsSmall.txt. Modify your DataCollector initialization
@@ -33,7 +27,9 @@ public class TargetedAd {
 
 
     /* your code here */
-    
+    DataCollector d = new DataCollector();
+    d.setData();
+  
      
   }
 
